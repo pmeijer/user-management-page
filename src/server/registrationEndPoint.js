31d353b1d2a3e7c9c15ef2f1419135f38ddac853
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-06-29 18:07:07 -0500

    Add a disabled user with the given data.

--- src/server/registrationEndPoint.js
+++ src/server/registrationEndPoint.js
@@ -1,52 +1,63 @@
 /*jshint node:true, camelcase:false*/
 /**
- * This is the default registration end point. It either allows or disallows users to register.
- * It will be used if gmeConfig.authentication.allowUserRegistration is either false or true.
- * To plugin your deployment specific end-point set gmeConfig.authentication.allowUserRegistration to
- * a path to a module with the same signature as this.
  *
  * @author pmeijer / https://github.com/pmeijer
  */
 
+var Q = require('q');
+
 'use strict';
 module.exports = function getRegistrationEndPoint(middlewareOpts) {
     var gmeConfig = middlewareOpts.gmeConfig,
-        gmeAuth = middlewareOpts.gmeAuth;
+        gmeAuth = middlewareOpts.gmeAuth,
+        logger = middlewareOpts.logger.fork('GSN-Registration');
 
 
     return function (req, res, next) {
-        var receivedData = req.body;
+        var receivedData = req.body,
+            logData = JSON.stringify(JSON.stringify(receivedData));
+
+        delete logData.password;
 
         if (gmeConfig.authentication.enable === false) {
             res.sendStatus(404);
             return;
         }
 
+        logger.info('New user request!', logData);
+
         // TODO: Add regex for userId and check other data too.
         if (typeof receivedData.userId !== 'string' || receivedData.userId.length === 0 ||
             typeof receivedData.email !== 'string' || receivedData.email.length === 0) {
+
+            logger.warn('Provided data was invalid!');
             res.sendStatus(400);
             return;
         }
 
-        gmeAuth.listUsers()
+        gmeAuth.listUsers({disabled: undefined, email: receivedData.email})
             .then(function (currentUsers) {
-                var i;
-
-                for (i = 0; i < currentUsers.length; i += 1) {
-                    if (currentUsers[i]._id === receivedData.userId) {
-                        throw new Error('UserID [' + receivedData.userId + '] is already used!');
-                    } else if (currentUsers[i].email === receivedData.email) {
-                        throw new Error('Email [' + receivedData.email + '] is already used!');
-                    }
+                if (currentUsers.length !== 0) {
+                    throw new Error('Email [' + receivedData.email + '] is already registered!');
                 }
 
-                console.log('New user request!', JSON.stringify(receivedData, null, 2));
-
+                return gmeAuth.addUser(receivedData.userId, receivedData.email, receivedData.password, true, {
+                    disabled: true,
+                    data: {
+                        userName: receivedData.userName,
+                        orgName: receivedData.orgName,
+                        orgAddr: receivedData.orgAddr,
+                        orgCountry: receivedData.orgCountry
+                    }
+                });
+            })
+            .then(function () {
                 res.sendStatus(200);
             })
             .catch(function (err) {
-                if (err.message.indexOf('] is already used!') > -1) {
+                if (err.message.indexOf('] is already registered!') > -1 ||
+                    err.message.indexOf('user already exists') > -1) {
+                    logger.error(err);
                     res.sendStatus(400);
                     return;
                 }

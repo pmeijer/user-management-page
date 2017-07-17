73f66ce4dd43ed419bdfd5dec44a895fd7d1a4bc
Nag <nag@isis.vanderbilt.edu>
2017-07-17 11:47:01 -0500

    updated disclaimer

--- src/common/components/login/TermsAndConditions.jsx
+++ src/common/components/login/TermsAndConditions.jsx
@@ -28,10 +28,11 @@ export default class TermsAndConditions extends Component {
                 <b>Disclaimer</b>
             </p>
             <div className="terms-and-conditions-text">
-                The contents of the <i>Model Based Assurance</i> website have been prepared for the <i>Radiation Effects and
-                Reliability Assurance</i> research community for informational purposes that are not export controlled.
-                Your privacy and security are important to us; please <b>do not</b> upload any data that is <b>controlled
-                unclassified information, export controlled, or considered to be intellectual property</b>.
+	    <p>
+		The SEAM tool set and the associated models have been prepared for the Radiation Effects research community for informational purposes that are not export controlled. Your privacy and security are important to us; please <b>do not<.b> upload any data that is <b>controlled unclassified information, export controlled, or considered to be intellectual property</b>.<br>
+		Note: The content of this website  and the SEAM tool set are considered “Beta Software”.<br>
+ 	        Vanderbilt University disclaims all warranties with regard to this software, including all implied warranties of merchantability and fitness.  In no event shall Vanderbilt University be liable for any special, indirect or consequential damages or any damages whatsoever resulting from loss of use, data or profits, whether in an action of contract, negligence or other tortious action, arising out of or in connection with the use or performance of this software.
+	     </p>
             </div>
         </div>;
     }
